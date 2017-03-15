cc132b4f3a617006e4de1695a75fc20789b40006
Thomas von Deyen <thomas@vondeyen.com>
2017-03-15 23:08:43 +0100

    Update review settings layout to Solidus UI
    Expand the settings tab when review settings form is open and adjust the form to fit into the Solidus settings section

--- app/overrides/add_reviews_to_admin_configuration_sidebar.rb
+++ app/overrides/add_reviews_to_admin_configuration_sidebar.rb
@@ -1,5 +1,11 @@
-Deface::Override.new(:virtual_path => "spree/admin/shared/_configuration_menu",
-                     :name => "converted_admin_configurations_menu",
-                     :insert_bottom => "[data-hook='admin_configurations_sidebar_menu'], #admin_configurations_sidebar_menu[data-hook]",
-                     :text => "<%= configurations_sidebar_menu_item Spree.t('spree_reviews.review_settings'), edit_admin_review_settings_path %>",
-                     :disabled => false)
+Spree::Backend::Config.configure do |config|
+  config.menu_items.detect { |menu_item|
+    menu_item.label == :settings
+  }.sections << :review_settings
+end
+
+Deface::Override.new(virtual_path: "spree/admin/shared/_settings_sub_menu",
+                     name: "converted_admin_configurations_menu",
+                     insert_bottom: "[data-hook='admin_settings_sub_tabs']",
+                     text: "<%= tab :reviews, url: edit_admin_review_settings_path, match_path: /review_settings/ %>",
+                     disabled: false)
