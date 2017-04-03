ca48edd139ca96623b7e9c331fbed11bfa99511f
Evgeny Sugakov <shiroginne@gmail.com>
2017-04-03 20:04:41 +0200

    Remove bar-rating JS and CSS from backend

--- app/assets/javascripts/spree/backend/solidus_reviews.js
+++ app/assets/javascripts/spree/backend/solidus_reviews.js
@@ -1,9 +1,1 @@
-//= require jquery.barrating
 //= require spree/backend
-
-$(document).on('ready', function () {
-  $.each($("select.review-stars"), function(index, value) {
-    selectTag = $(value)
-    selectTag.barrating(selectTag.data("barrating-options"))
-  });
-});
