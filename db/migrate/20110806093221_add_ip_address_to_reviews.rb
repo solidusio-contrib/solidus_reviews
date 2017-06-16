baea99adcfee06cccb00e4fc55323d3ec4a9d88a
Dustin Grange <dustin@grange.io>
2017-06-16 11:53:05 -0600

    Add SolidusSupport for migration compatibility.
    Rails 5.1 removes the ability of inheriting from ActiveRecord::Migration
    directly but we still need it for solidus version using pre Rails 5.0 versions.

--- db/migrate/20110806093221_add_ip_address_to_reviews.rb
+++ db/migrate/20110806093221_add_ip_address_to_reviews.rb
@@ -1,4 +1,4 @@
-class AddIpAddressToReviews < ActiveRecord::Migration
+class AddIpAddressToReviews < SolidusSupport::Migration[4.2]
   def self.up
     add_column :reviews, :ip_address, :string
   end
