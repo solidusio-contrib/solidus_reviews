362c80ae64a5ae66482aa21d8c8a62976c725ac0
Alessandro Desantis <desa.alessandro@gmail.com>
2019-11-27 10:10:21 +0100

    Rename spree_reviews path to solidus_reviews

--- lib/solidus_reviews.rb
+++ lib/solidus_reviews.rb
@@ -4,6 +4,8 @@ require 'solidus'
 require 'solidus_support'
 require 'sassc/rails'
 require 'deface'
-require 'spree_reviews/engine'
-require 'solidus_reviews/version'
 require 'coffee_script'
+
+require 'solidus_reviews/engine'
+require 'solidus_reviews/version'
+
--- lib/solidus_reviews/engine.rb
+++ lib/solidus_reviews/engine.rb
@@ -1,16 +1,16 @@
 # frozen_string_literal: true
 
-module SpreeReviews
+module SolidusReviews
   class Engine < Rails::Engine
     require 'spree/core'
-    isolate_namespace Spree
+    isolate_namespace ::Spree
     engine_name 'solidus_reviews'
 
     def self.activate
       Dir.glob(File.join(File.dirname(__FILE__), '../../app/**/*_decorator*.rb')) do |c|
         Rails.configuration.cache_classes ? require(c) : load(c)
       end
-      Spree::Ability.register_ability(Spree::ReviewsAbility)
+      ::Spree::Ability.register_ability(::Spree::ReviewsAbility)
     end
 
     if SolidusSupport.api_available?
