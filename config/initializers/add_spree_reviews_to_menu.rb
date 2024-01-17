cad545863c346b8ef6dc04ddb40c90013ff05270
Martin Meyerhoff <mamhoff@gmail.com>
2024-01-17 10:21:25 +0100

    Use Menu item API instead of Deface for reviews admin
    If we do not need to be using Deface, we should not.

--- config/initializers/add_spree_reviews_to_menu.rb
+++ config/initializers/add_spree_reviews_to_menu.rb
@@ -3,18 +3,29 @@
 Rails.application.config.to_prepare do
   Spree::Backend::Config.configure do |config|
     config.menu_items = config.menu_items.map do |item|
-      next item unless item.label.to_sym == :settings
-
-      # The API of the MenuItem class changes in Solidus 4.2.0
-      if item.respond_to?(:children)
-        item.children << Spree::BackendConfiguration::MenuItem.new(
-          label: :reviews,
-          condition: -> { can?(:admin, Spree::ReviewsConfiguration) },
-          url: -> { Spree::Core::Engine.routes.url_helpers.edit_admin_review_settings_path },
-          match_path: /review_settings/
-        )
-      else
-        item.sections << :reviews
+      if item.label.to_sym == :settings
+        # The API of the MenuItem class changes in Solidus 4.2.0
+        if item.respond_to?(:children)
+          item.children << Spree::BackendConfiguration::MenuItem.new(
+            label: :reviews,
+            condition: -> { can?(:admin, Spree::ReviewsConfiguration) },
+            url: -> { Spree::Core::Engine.routes.url_helpers.edit_admin_review_settings_path },
+            match_path: /review_settings/
+          )
+        else
+          item.sections << :reviews
+        end
+      elsif item.label.to_sym == :products
+        if item.respond_to?(:children)
+          item.children << Spree::BackendConfiguration::MenuItem.new(
+            label: :reviews,
+            condition: -> { can?(:admin, Spree::Review) },
+            url: -> { Spree::Core::Engine.routes.url_helpers.admin_reviews_path },
+            match_path: /reviews/
+          )
+        else
+          item.sections << :reviews
+        end
       end
       item
     end
