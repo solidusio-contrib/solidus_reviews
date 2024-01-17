c4ff98b17367d5a24217de9794ad4c607f7d3e02
Martin Meyerhoff <mamhoff@gmail.com>
2024-01-17 10:56:03 +0100

    Move menu configuration into initializer
    This initializer will run even if the gem is not installed in the
    routes, and break otherwise.
    
    This will work for new installs.

--- lib/generators/solidus_reviews/install/templates/initializer.rb
+++ lib/generators/solidus_reviews/install/templates/initializer.rb
@@ -1,5 +1,35 @@
 # frozen_string_literal: true
 
+Spree::Backend::Config.configure do |config|
+  config.menu_items = config.menu_items.map do |item|
+    if item.label.to_sym == :settings
+      # The API of the MenuItem class changes in Solidus 4.2.0
+      if item.respond_to?(:children)
+        item.children << Spree::BackendConfiguration::MenuItem.new(
+          label: :reviews,
+          condition: -> { can?(:admin, Spree::ReviewsConfiguration) },
+          url: -> { Spree::Core::Engine.routes.url_helpers.edit_admin_review_settings_path },
+          match_path: /review_settings/
+        )
+      else
+        item.sections << :reviews
+      end
+    elsif item.label.to_sym == :products
+      if item.respond_to?(:children)
+        item.children << Spree::BackendConfiguration::MenuItem.new(
+          label: :reviews,
+          condition: -> { can?(:admin, Spree::Review) },
+          url: -> { Spree::Core::Engine.routes.url_helpers.admin_reviews_path },
+          match_path: /reviews/
+        )
+      else
+        item.sections << :reviews
+      end
+    end
+    item
+  end
+end
+
 SolidusReviews.configure do |config|
   # TODO: Remember to change this with the actual preferences you have implemented!
   # config.sample_preference = 'sample_value'
