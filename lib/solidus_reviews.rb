c3c46dcf53b5a609f94756d2b1b3650c0d8db3e1
Martin Meyerhoff <mamhoff@gmail.com>
2025-01-14 09:31:59 +0100

    Use SolidusSupport instead of Flickwerk

--- lib/solidus_reviews.rb
+++ lib/solidus_reviews.rb
@@ -3,7 +3,6 @@
 require 'solidus_core'
 require 'solidus_reviews/configuration'
 require 'solidus_support'
-require 'flickwerk'
 require 'deface'
 
 require 'solidus_reviews/version'
--- lib/solidus_reviews/engine.rb
+++ lib/solidus_reviews/engine.rb
@@ -7,28 +7,6 @@ module SolidusReviews
   class Engine < Rails::Engine
     include SolidusSupport::EngineExtensions
 
-    include Flickwerk
-
-    Flickwerk.aliases["Spree.user_class"] = Spree.user_class_name
-
-    if SolidusSupport.api_available?
-      config.autoload_paths += root.join("lib", "patches", "api").glob("*")
-    end
-
-    initializer "solidus_reviews_api_patches", before: "flickwerk.add_patch_paths" do
-      patch_path = root.join("lib", "patches", "api")
-      Flickwerk.patch_paths += [patch_path]
-    end
-
-    if SolidusSupport.frontend_available?
-      config.autoload_paths += root.join("lib", "patches", "frontend").glob("*")
-    end
-
-    initializer "solidus_reviews_frontend_patches", before: "flickwerk.add_patch_paths" do
-      patch_path = root.join("lib", "patches", "frontend")
-      Flickwerk.patch_paths += [patch_path]
-    end
-
     isolate_namespace ::Spree
 
     engine_name 'solidus_reviews'
